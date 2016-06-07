c5ba90a0a6c36841d51b0447524b03df8c4f0a76
Fabricio Valadares <valadares.fabricio@gmail.com>
2016-06-07 12:04:54 -0300

    Update

--- src/Number.php
+++ src/Number.php
@@ -27,7 +27,7 @@ class Number {
         $valInteger->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 0);
         return $valInteger->format((float) $value, \NumberFormatter::TYPE_INT64);
     }
-    
+
     /**
      * Formatação de numero formato moeda
      * @param float/integer $value
@@ -41,7 +41,7 @@ class Number {
         $valCurrency->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $precision);
         return $valCurrency->format((float) $value, \NumberFormatter::TYPE_DOUBLE);
     }
-    
+
     /**
      * Formatação de numero decimal
      * @param float/integer $value
@@ -54,6 +54,39 @@ class Number {
         $valDecimal->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, $precision);
         $valDecimal->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $precision);
         return $valDecimal->format((float) $value, \NumberFormatter::TYPE_DOUBLE);
+    }
+
+    /**
+     * Formatação de numero em bytes para o formato de tamanho
+     * @param int $bytes
+     * @param string $unit
+     * @param int $decimals
+     * @return string
+     */
+    public static function byteFormat($bytes, $unit = "", $decimals = 2) {
+        $units = array('B' => 0, 'KB' => 1, 'MB' => 2, 'GB' => 3, 'TB' => 4, 'PB' => 5, 'EB' => 6, 'ZB' => 7, 'YB' => 8);
+
+        $value = 0;
+        if ($bytes > 0) {
+            // Generate automatic prefix by bytes 
+            // If wrong prefix given
+            if (!array_key_exists($unit, $units)) {
+                $pow = floor(log($bytes) / log(1024));
+                $unit = array_search($pow, $units);
+            }
+
+            // Calculate byte value by prefix
+            $value = ($bytes / pow(1024, floor($units[$unit])));
+        }
+
+        // If decimals is not numeric or decimals is less than 0 
+        // then set default value
+        if (!is_numeric($decimals) || $decimals < 0) {
+            $decimals = 2;
+        }
+
+        // Format output
+        return sprintf('%.' . $decimals . 'f ' . $unit, $value);
     }
 
 }
